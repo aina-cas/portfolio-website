ca6853d79245838245f73bc0c6130567e98e73ba
aina-cas <ainamaplearts@gmail.com.noreply.github.com>
2023-01-17 10:58:15 +0100

    Add animation to hero

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+// Balls animation  
+
+
+// Colors for balls 
+
+const colors = ['#f05252', '#f2711c', '#f5a623', '#f7d12e', '#a3e048', '#56d9cd', '#1fb8e5', '#1a75bb', '#5151e5', '#a54ee5', '#f05252'];
+
+const numBalls = 100;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  const hero = document.querySelector('.hero');
+  hero.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
